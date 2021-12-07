b2a40b6f3073d38bfdf019e47a30d962d6380029
ophlop <95183292+ophlop@users.noreply.github.com>
2021-12-07 08:57:32 +0300

    Update rashchet.js

--- rashchet.js
+++ rashchet.js
@@ -92,7 +92,7 @@ function init() {
       
       return finalPrice;
     }
-var finalPrice
+/*var finalPrice
 document.getElementsBytagname('t-input-subtitle')[0].innerHTML = finalPrice;
 }
 </script>
